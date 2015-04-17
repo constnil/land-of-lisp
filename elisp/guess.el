ee0bc3fd4d0cbe1f86310971768d0bfa939c5a52
Tiefeng Wu <icebergwtf@qq.com>
2015-04-17 18:12:25 +0800

    change directories from game to language

---
+++ elisp/guess.el
@@ -0,0 +1,22 @@
+;;; guess.el --- "Guess" game from "Land of Lisp" by Conrad Barski, M.D.
+
+;; Copyright (C) 2015, Wu Tiefeng.
+
+;; Author: Wu Tiefeng <IcebergWTF@qq.com>
+;; Maintainer: Wu Tiefeng
+;; Keywords: games
+
+;;; Code:
+
+(defun guess-my-number (num)
+  "start a guess game"
+  (interactive "p")
+  (message "TBD"))
+
+(defun bigger ())
+
+(defun smaller ())
+
+(provide 'guess)
+
+;;; guess.el ends here
