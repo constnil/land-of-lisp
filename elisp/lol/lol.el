2fe7bf626d20947975bd37d907bdf5b122985c0d
Tiefeng Wu <icebergwtf@qq.com>
2017-06-07 14:59:33 +0800

    create lisp directory in elisp/lol/ to hold game codes

---
+++ elisp/lol/lol.el
@@ -0,0 +1,17 @@
+;;; lol.el --- Land of Lisp games in emacs lisp.
+
+;; Author: Wu Tiefeng <IcebergWTF@qq.com>
+;; Maintainer: Wu Tiefeng
+;; Keywords: games
+
+;;; Commentary:
+
+;;; Code:
+
+(add-to-list 'load-path (expand-file-name "lisp/"))
+
+(require 'guess)
+
+(provide 'lol)
+
+;;; lol.el ends here
