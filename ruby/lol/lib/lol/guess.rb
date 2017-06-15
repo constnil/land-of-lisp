4deb97b763a4b29a99b2cb186ec12019cc22595e
Tiefeng Wu <icebergwtf@qq.com>
2017-06-15 17:45:04 +0800

    add ruby guess game

---
+++ ruby/lol/lib/lol/guess.rb
@@ -0,0 +1,22 @@
+$small = 1
+$big = 100
+
+def guess_my_number()
+  ($big + $small) >> 1
+end
+
+def smaller
+  $big = guess_my_number
+  guess_my_number
+end
+
+def bigger
+  $small = guess_my_number
+  guess_my_number
+end
+
+def start_over
+  $small = 1
+  $big = 100
+  guess_my_number
+end
